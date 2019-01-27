2ab23292b5f3a749ae2852c575c90c18d5769929
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-01-26 19:28:18 -0600

    added the whole table to the seeder file

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,1 +1,39 @@
 USE codeup_test_db;
+
+--insert into /tableName/ (field1, field2...) values ('value1', 'value2'...);
+
+
+insert into albums (artist, name, release_date, genre, sales) values
+ ('Michael Jackson','Thriller',1982,'Pop',47.3),
+ ('Eagles','Greatest Hits', 1976, 'Country Rock', 41.2),
+ ('AC/DC','Back in black', 1980,'Hard Rock', 26.1),
+ ('Pink Floyd','The Dark Side of the Moon', 1973,'Progressive Rock', 24.2),
+ ('Whitney Houston','The Bodyguard', 1992,'R&B', 28.4),
+ ('Meat Loaf','Bat Out Of Hell', 1977,'Hard Rock', 21.5),
+ ('Eagles','Hotel California', 1976,'Soft Rock', 31.5),
+ ('Bee Gees','Saturday Night Fever', 1977,'Disco', 21.6),
+ ('Fleetwood Mac','Rumors', 1977,'Soft Rock', 27.9),
+ ('Shania Twain','Come On Over', 1997,'Country Pop', 29.6),
+ ('Olivia Newton John','Grease: The original soundtrack', 1978,'Rock and Roll', 14.4),
+ ('Led Zeppelin','Led Zeppelin IV', 1971,'Hard Rock', 29.0),
+ ('Michael Jackson','Bad', 1987,'Pop', 19.3),
+ ('Alanis Morissette','Jagged Little Pill', 1995,'Alternative Rock', 24.4),
+ ('Celine Dion','Falling into You', 1996,'Pop', 20.2),
+ ('The Beatles','Sgt.Peppers Lonely Hearts Club Band', 1967,'Rock',18.2),
+ ('Various Artists','Dirty Dancing', 1987,'Pop Rock', 17.9),
+ ('Michael Jackson','Dangerous', 1991,'Rock Funk', 17.3),
+ ('Adele','21', 2011,'Pop', 25.3),
+ ('Celine Dion','Lets Talk about Love', 1997,'Pop', 19.3),
+ ('The Beatles','1', 2000,'Rock', 23.4),
+ ('ABBA','Gold: Greatest Hits', 1992,'Pop Disco', 23.0),
+ ('Madonna','Immaculate Collection', 1990,'Pop', 19.4),
+ ('The Beatles','Abbey Road', 1969,'Rock', 14.4),
+ ('Bruce Springsteen','Born in the U.S.A', 1984,'Rock', 19.6),
+ ('Dire Straits','Brothers in Arms', 1985,'Roots Rock', 17.7),
+ ('James Horner','Titanic soundtrack', 1997,'Film Score', 18.1),
+ ('Metallica','Metallica', 1991,'Heavy Metal', 25.2),
+ ('Nirvana','Nevermind', 1991,'Grunge', 16.7),
+ ('Pink Floyd','The Wall', 1979,'Progressive Rock', 18.6),
+ ('Santana','Supernatural', 1999,'Latin Rock', 20.5),
+ ('Guns N Roses','Appetite for Destruction', 1987,'Hard Rock', 21.6);
+
