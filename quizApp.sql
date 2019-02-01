17d6758134051ec510852ca5705a6dff7814546d
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-02-01 09:47:07 -0600

    finished the quiz app

--- quizApp.sql
+++ quizApp.sql
@@ -2,7 +2,7 @@ DROP DATABASE quizApp;
 
 CREATE DATABASE IF NOT EXISTS quizApp;
 
-use quizApp;
+USE quizApp;
 
 CREATE TABLE IF NOT EXISTS admins (
 admin_id INT UNSIGNED NOT NULL,
@@ -13,30 +13,82 @@ PRIMARY KEY (admin_id)
 
 CREATE TABLE IF NOT EXISTS students (
 student_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (student_id),
 student_first_name VARCHAR(20),
 student_last_name VARCHAR(20),
-PRIMARY KEY (student_id)
+quiz_score INT UNSIGNED NOT NULL,
+quiz_id INT UNSIGNED NOT NULL
 );
+
+ALTER TABLE students
+ADD FOREIGN KEY (quiz_id) REFERENCES quiz (quiz_id;
+
 
 CREATE TABLE IF NOT EXISTS quizes (
 quiz_id INT UNSIGNED NOT NULL,
-question_number INT UNSIGNED NOT NULL,
-question TEXT NOT NULL,
+quiz_number INT UNSIGNED NOT NULL,
+PRIMARY KEY (quiz_id)
+);
+
+CREATE TABLE IF NOT EXISTS quiz (
+quiz_id INT UNSIGNED NOT NULL,
 PRIMARY KEY (quiz_id),
 admin_id INT UNSIGNED NOT NULL,
 answer_id INT UNSIGNED NOT NULL,
-FOREIGN KEY (admin_id) REFERENCES admins (admin_id),
-FOREIGN KEY (answer_id) REFERENCES answers (answer_id)
+question_id INT UNSIGNED NOT NULL
 );
 
--- add a quiz table later
+ALTER TABLE quiz
+ADD FOREIGN KEY (admin_id) REFERENCES admins (admin_id);
+ALTER TABLE quiz
+ADD FOREIGN KEY (answer_id) REFERENCES answers (answer_id);
+ALTER TABLE quiz
+ADD FOREIGN KEY (question_id) REFERENCES questions (question_id);
+
+
+CREATE TABLE IF NOT EXISTS questions (
+question_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (question_id),
+quiz_id INT UNSIGNED NOT NULL,
+question TEXT NOT NULL,
+answer_id INT UNSIGNED NOT NULL
+);
+ALTER TABLE questions
+ADD FOREIGN KEY (quiz_id) REFERENCES quiz (quiz_id);
+ALTER TABLE questions
+ADD FOREIGN KEY (answer_id) REFERENCES answers (answer_id);
+
+
+CREATE TABLE IF NOT EXISTS answer_key(
+answer_key_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (answer_key_id),
+answer_id INT UNSIGNED NOT NULL,
+quiz_id INT UNSIGNED NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS answers (
 answer_id INT UNSIGNED NOT NULL,
 answer TEXT NOT NULL,
 PRIMARY KEY (answer_id),
 quiz_id INT UNSIGNED NOT NULL,
-FOREIGN KEY (quiz_id) REFERENCES quizes (quiz_id)
+question_id INT UNSIGNED NOT NULL
+);
+ALTER TABLE answers
+ADD FOREIGN KEY (quiz_id) REFERENCES quizes (quiz_id);
+ALTER TABLE answers
+ADD FOREIGN KEY (question_id) REFERENCES questions (question_id);
+
+CREATE TABLE IF NOT EXISTS student_answers (
+sa_id INT UNSIGNED NOT NULL AUTO_INCREMENT, -- sa stands for student answer
+correct_answer INT UNSIGNED NOT NULL,
+attempted_answer TEXT NOT NULL,
+PRIMARY KEY (sa_id),
+student_id INT UNSIGNED NOT NULL,
+quiz_id INT UNSIGNED NOT NULL,
+question_id INT UNSIGNED NOT NULL,
+FOREIGN KEY (student_id) REFERENCES students (student_id),
+FOREIGN KEY (quiz_id) REFERENCES quiz (quiz_id),
+FOREIGN KEY (question_id) REFERENCES questions (question_id)
 );
 
 
--- quizAppSeeder.sql
+++ quizAppSeeder.sql
@@ -3,5 +3,26 @@ use quizApp;
 INSERT INTO admins (admin_id, admin_first_name, admin_last_name)
 VALUES (69,'Juan','Edwards');
 
-INSERT INTO quizes (quiz_id, question_number, question, admin_id)
-VALUES (900, 1,'Is landon a spy?', 69);
+INSERT INTO admins (admin_id, admin_first_name, admin_last_name)
+VALUES (700,'Josh','Hart');
+
+INSERT INTO admins (admin_id, admin_first_name, admin_last_name)
+VALUES (30,'Kevin','Durant');
+
+INSERT INTO admins (admin_id, admin_first_name, admin_last_name)
+VALUES (00,'Damian','Lillard');
+
+INSERT INTO students (student_id, student_first_name, student_last_name, quiz_score, quiz_id)
+VALUES (1121, 'Sam', 'Rodriguez', 88, 1);
+
+INSERT INTO students (student_id, student_first_name, student_last_name, quiz_score, quiz_id)
+VALUES (1331, 'Landon', 'Harvey', 67, 1);
+
+INSERT INTO students (student_id, student_first_name, student_last_name, quiz_score, quiz_id)
+VALUES (40404, 'Chris', 'Caldwell', 44, 2);
+
+INSERT INTO students (student_id, student_first_name, student_last_name, quiz_score, quiz_id)
+VALUES (0001, 'Ricky', 'Bobby', 75, 2);
+
+INSERT INTO quizes (quiz_id, quiz_number)
+VALUES (100, 1);
